550dacb900347b9b617c07736640fa711fc417d2
tomasz-wiaderek <t.wiaderek@gmail.com>
2021-05-06 20:02:55 +0200

    Singleton pattern implemented into LibraryData

--- classes/lib_data.py
+++ classes/lib_data.py
@@ -1,9 +1,21 @@
 from typing import Dict
-
+from threading import Lock
 from classes.book import Book
 
 
-class LibraryData:
+class SingletonMeta(type):
+    __instance = []
+    _lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls.__instance:
+                instance = super().__call__(*args, **kwargs)
+                cls.__instance = instance
+        return cls.__instance
+
+
+class LibraryData(metaclass=SingletonMeta):
 
     def __init__(self):
         self._books = {}
